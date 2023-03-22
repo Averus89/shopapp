2a3897ed56f9c2c149b66e8f4c69615b2763b101
Alan Żur <alanzur@gmail.com>
2023-03-22 19:20:36 +0100

    https://start.spring.io/

---
+++ src/test/java/pl/dexbtyes/shopapp/ShopappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dexbtyes.shopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
